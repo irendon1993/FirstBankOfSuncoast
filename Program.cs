22a6e7447681941579cd12383b326ea8d8a0a89d
Isaac Rendon <isaacrendon7@gmail.com>
2020-08-20 14:50:58 -0400

    Finished PEDAC began working on user class

--- Program.cs
+++ Program.cs
@@ -2,11 +2,65 @@
 
 namespace FirstBankOfSuncoast
 {
-  class Program
-  {
-    static void Main(string[] args)
+    // Create a Customer class
+    class Customer
     {
-      Console.WriteLine("Welcome to C#");
+        CheckingAccount()
+        {
+            CheckingAccountBalance =
+        }
+
+        SavingsAccount()
+        {
+            SavingsAccountBalance =
+      }
     }
-  }
+    class Program
+    {
+        // PROBLEM
+        // 
+        // Create a console app that allows a user to manage savings and checking banking transactions.
+        // A user will make a series of transactions.
+        // You will compute balances by examining all the transactions in the history
+        // The transactions will be saved in a file, using a CSV format to record the data.
+
+        // Example
+        // 
+        // For instance, if a user deposits 10 to their savings, then withdraws 8 from their savings,
+        // then deposits 25 to their checking, they have three transactions to consider. 
+        // Compute the checking and saving balance, using the transaction list, when needed. 
+        // In this case, their savings balance is 2 and their checking balance is 25.
+
+
+        // DATA
+        // 
+        // USER   
+        // CheckingAccount
+        // AccountBalance
+        // SavingsAccount
+        // AccountBalance
+        // TRANSACTION
+        // MONEY
+        // 
+        // A
+        // 
+        // Make a new object with properties:
+        // CheckingAccount
+        // SavingsAccount 
+        // Create a MENU
+        // Make option to VIEW balance
+        // Make option to WITHDRAW 
+        // Make option to DEPOSIT
+        // 
+        // 
+
+        // 
+        // 
+        //         
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C#");
+        }
+    }
 }
